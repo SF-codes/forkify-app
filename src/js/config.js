18c2e7fea88fac432700e38f80f3aea303bdbf09
ShanisFelicissimo <felicissimo.shanis@gmail.com>
2021-09-23 15:08:17 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ced143ea-8f7a-4b80-bca6-0994a0bedee9';
+export const MODAL_CLOSE_SEC = 2.5;
