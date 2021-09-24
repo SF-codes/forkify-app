84f11b052b1679e88f923a0c7810997bfeccfe46
ShanisFelicissimo <felicissimo.shanis@gmail.com>
2021-09-23 20:24:00 -0400

    Fix up

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,7 +8,7 @@ import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 
 // import 'core-js/stable';
-// import 'regenerator-runtime/runtime';
+import 'regenerator-runtime/runtime';
 
 ///////////////////////////////////////
 
